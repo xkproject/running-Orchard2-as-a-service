227f178e407c9178c7d317bf01e3c88a5819a2d9
Sergio Navarro <jersio@hotmail.com>
2017-01-11 22:08:06 +0100

    Initial Commit

---
+++ Orchard.Cms.Web/ProgramAsAService.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using DasMulli.Win32.ServiceUtils;
+using Microsoft.DotNet.InternalAbstractions;
+using System.IO;
+
+namespace Orchard.Cms.Web
+{
+    public class Program
+    {
+        public const string RunAsServiceFlag = "--run-as-service";
+        private const string RegisterServiceFlag = "--register-service";
+        private const string UnregisterServiceFlag = "--unregister-service";
+        private const string ServiceNameFlag = "--service-name";
+        private const string ServiceDysplayNameFlag = "--service-display-name";
+        private const string ServiceDescriptionFlag = "--service-description";
+        private const string HelpFlag = "--?";
+
+        private const string DefaultServiceName = "Orchard 2";
+        private const string DefaultServiceDisplayName = "Orchard 2";
+        private const string DefaultServiceDescription = "Orchard 2 CMS running on .NET Core";
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Contains(RunAsServiceFlag))
+                {
+                    RunAsService(args);
+                }
+                else if (args.Contains(RegisterServiceFlag))
+                {
+                    RegisterService();
+                }
+                else if (args.Contains(UnregisterServiceFlag))
+                {
+                    UnregisterService();
+                }
+                else if (args.Contains(HelpFlag))
+                {
+                    DisplayHelp();
+                }
+                else
+                {
+                    RunInteractive(args);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error ocurred: {ex.Message}");
+            }
+        }
+
+        private static void RunAsService(string[] args)
+        {
+            var appPath = ApplicationEnvironment.ApplicationBasePath;
+            while (!File.Exists(appPath + "\\web.config"))
+            {
+                appPath = Directory.GetParent(appPath).FullName;
+            }
+
+            System.IO.Directory.SetCurrentDirectory(appPath);
+            var orchardService = new OrchardCmsWin32Service(commandLineArguments: args.Where(a => a != RunAsServiceFlag).ToArray(), useInteractiveCommandLine: false);
+            var serviceHost = new Win32ServiceHost(orchardService);
+            serviceHost.Run();
+        }
+
+        private static void RunInteractive(string[] args)
+        {
+            var orchardService = new OrchardCmsWin32Service(commandLineArguments: args, useInteractiveCommandLine: true);
+            orchardService.Start(new string[0], () => { });
+            orchardService.Stop();
+        }
+
+        private static void RegisterService()
+        {
+            // Environment.GetCommandLineArgs() includes the current DLL from a "dotnet my.dll --register-service" call, which is not passed to Main()
+            var remainingArgs = System.Environment.GetCommandLineArgs()
+                .Where(arg => arg != RegisterServiceFlag 
+                                    && !arg.StartsWith(ServiceNameFlag) 
+                                    && !arg.StartsWith(ServiceDysplayNameFlag)
+                                    && !arg.StartsWith(ServiceDescriptionFlag))
+                .Select(EscapeCommandLineArgument)
+                .Append(RunAsServiceFlag);
+            
+            var host = Process.GetCurrentProcess().MainModule.FileName;
+            if (!host.EndsWith("dotnet.exe", StringComparison.OrdinalIgnoreCase))
+            {
+                // For self-contained apps, skip the dll path
+                remainingArgs = remainingArgs.Skip(1);
+            }
+
+            string serviceName = GetServiceName();
+            string serviceDisplayName = GetServiceDysplayName();
+            string serviceDescription = GetServiceDescription();
+            var fullServiceCommand = host + " " + string.Join(" ", remainingArgs);
+            new Win32ServiceManager()
+              .CreateService(serviceName, serviceDisplayName, serviceDescription, fullServiceCommand, new Win32ServiceCredentials("NT SERVICE\\" + serviceName, null), autoStart: true, startImmediately: false, errorSeverity: ErrorSeverity.Normal);
+
+
+
+            Console.WriteLine($@"Successfully registered ""{serviceDisplayName}"" (""{serviceDescription}"")");
+        }
+
+        private static string GetServiceName()
+        {
+            var serviceNameArg = System.Environment.GetCommandLineArgs().FirstOrDefault(arg => arg.StartsWith(ServiceNameFlag)) ?? ServiceNameFlag + ":" + DefaultServiceName;
+            return serviceNameArg.Substring(ServiceNameFlag.Length + 1);
+        }
+
+        private static string GetServiceDysplayName()
+        {
+            var serviceDisplayNameArg = System.Environment.GetCommandLineArgs().FirstOrDefault(arg => arg.StartsWith(ServiceDysplayNameFlag)) ?? ServiceDysplayNameFlag + ":" + DefaultServiceDisplayName;
+            return serviceDisplayNameArg.Substring(ServiceDysplayNameFlag.Length + 1);
+        }
+
+        private static string GetServiceDescription()
+        {
+            var serviceDescriptionArg = System.Environment.GetCommandLineArgs().FirstOrDefault(arg => arg.StartsWith(ServiceDescriptionFlag)) ?? ServiceDescriptionFlag + ":" + DefaultServiceDescription;
+            return serviceDescriptionArg.Substring(ServiceDescriptionFlag.Length + 1);
+        }
+
+        private static void UnregisterService()
+        {
+            string serviceName = GetServiceName();
+            string serviceDisplayName = GetServiceDysplayName();
+            new Win32ServiceManager()
+                                    .DeleteService(serviceName);
+            Console.WriteLine("Successfully unregistered service");
+        }
+
+        private static void DisplayHelp()
+        {
+            Console.WriteLine(DefaultServiceDescription);
+            Console.WriteLine();
+            Console.WriteLine("Use one of the following options for running Orchard as a windows service or standalone:");
+            Console.WriteLine("  --register-service        Registers and starts this program as a windows service named \"" + DefaultServiceDisplayName + "\"");
+            Console.WriteLine("                            All additional arguments will be passed to ASP.NET Core's WebHostBuilder.");
+            Console.WriteLine("  --unregister-service      Removes the windows service created by --register-service.");
+            Console.WriteLine("  --service-name:<your service name>     Replace default service name");
+            Console.WriteLine("  --service-display-name:<your service name>     Replace default service display-name");
+            Console.WriteLine("  --service-description:<your service description>     Replace default service description");
+        }
+
+        private static string EscapeCommandLineArgument(string arg)
+        {
+            // http://stackoverflow.com/a/6040946/784387
+            arg = Regex.Replace(arg, @"(\\*)" + "\"", @"$1$1\" + "\"");
+            arg = "\"" + Regex.Replace(arg, @"(\\+)$", @"$1$1") + "\"";
+            return arg;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# running-Orchard2-as-a-service
-Including following classes in Orchard2 web project allows to execute it as a windows service
+# Running Orchard2 as a service
+Orchard 2 by default is compiled to be portable. It provides pros but also cons. 
+One of the cons is we cannot use methods available in .NET Core for executing a program as a service because those are platform dependent.
+This limitation can be overcome thanks to [dasMulli/dotnet-win32-service|https://github.com/dasMulli/dotnet-win32-service] library.
+
+In this repo you can find the extra files needed for executing Orchard 2 as a service.
++ *ProgramAsAService.cs*: It replaces default Orchard2 Program.cs.
++ *OrchardCmsWin32Service.cs*:  It implements IWin32Service.
+ alternate Program.cs file called ProgramAsAService.cs It replaces default Program.cs.
+
+ Add those two files within the Orchard.Cms.Web project in your Orchard 2 solution.
+
+Next in project.json yoy need to do following changes:
++ Add these two dependencies:
+`"DasMulli.Win32.ServiceUtils": "1.0.1"`
+`"Microsoft.Extensions.Configuration.CommandLine": "1.0.0"`
++ Add an this entry `"excludeFiles": [ "Program.cs" ]` to buildoptions -> compile to ignore default Command class.
+
+The following are the new arguments available to execute Orchard 2 through command line:");
+  --register-service	Registers and starts the program as a windows service. All additional arguments will be passed to ASP.NET Core's WebHostBuilder.
+	--service-name:<your service name>     Replace default service name (Orchard 2). Service will run using a virtual account named NT SERVICE\\<serviceName>.
+    --service-display-name:<your service name>     Replace default service display-name (Orchard 2)
+    --service-description:<your service description>     Replace default service description
+  --unregister-service      Removes the windows service created by --register-service.
+  
+After registering the service don't forget to provide read/write permissions in Orchard2 folder for vitual user NT SERVICE\\<serviceName> 
+
+##Samples
+
+Running Orchard 2 standalone:
+`dotnet Orchard.Cms.Web.dll --urls http://localhost`
+
+Registering Orchard 2 as a service:            
+`dotnet Orchard.Cms.Web.dll --register-service --urls http://localhost`
+
+Registering Orchard 2 as a service customizing its name:            
+`dotnet Orchard.Cms.Web.dll --register-service --urls http://localhost`
